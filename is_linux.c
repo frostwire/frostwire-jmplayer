d8eadec0e74d8ca054ccbd8c84e7e2f281519db5
Angel Leon <gubatron@gmail.com>
2023-01-16 15:37:18 -0700

    is_linux.c fixed to not do a false positive when running on macos

--- is_linux.c
+++ is_linux.c
@@ -4,8 +4,13 @@
 #include <stdio.h>
 int main() {
 #if defined(__linux__)
-  printf("is_linux:1\n");  
-  return 1;
+  #if defined(__APPLE__)
+    printf("is_linux:0\n");
+    return 0;
+  #else
+    printf("is_linux:1\n");
+    return 1;
+  #endif
 #else
   printf("is_linux:0\n");
   return 0;
