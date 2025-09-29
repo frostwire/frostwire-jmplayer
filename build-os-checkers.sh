646749efb80147e1ad154933112b983f65336672
Angel Leon <gubatron@gmail.com>
2025-09-29 14:44:47 +0000

    fixed compilation of prepare-ffmpeg-flags.c in ubuntu

--- build-os-checkers.sh
+++ build-os-checkers.sh
@@ -1,3 +1,3 @@
 gcc is_linux.c -o is_linux
 gcc is_macos.c -o is_macos
-gcc -std=c11 prepare-ffmpeg-flags.c -o prepare-ffmpeg-flags
+gcc -std=c11 -D_POSIX_C_SOURCE=200809L prepare-ffmpeg-flags.c -o prepare-ffmpeg-flags
--- prepare-ffmpeg-flags.c
+++ prepare-ffmpeg-flags.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
